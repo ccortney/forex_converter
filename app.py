c766037d7b0126a16bdeb019cd2f640411f65d17
Camilla Cortney <camilla.cortney@gmail.com>
2021-12-20 17:17:21 -0800

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from forex_functions import conversion, get_symbol, check_currency_code
+import forex_python.converter
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = '1234'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+
+@app.route('/convert')
+def convert():
+    curr_from = request.args["curr-from"]
+    curr_to = request.args["curr-to"]
+    amount = request.args["amount"]
+    try:
+        new_amount = conversion(curr_from, curr_to, amount)
+        symbol = get_symbol(curr_to)
+        return render_template("result.html", new_amount=new_amount, symbol=symbol)
+    except ValueError:
+        flash(f"Invalid Amount: {amount}")
+    except forex_python.converter.RatesNotAvailableError:
+        if (check_currency_code(curr_from) == False):
+            flash(f"Invalid Currency Code: {curr_from}")
+        if (check_currency_code(curr_to) == False):
+            flash(f"Invalid Currency Code: {curr_to}")
+    return redirect('/')
+    
--- forex_functions.py
+++ forex_functions.py
@@ -1,2 +1,20 @@
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates, CurrencyCodes
 
+c = CurrencyRates()
+s = CurrencyCodes()
+
+def conversion(curr_from, curr_to, amount):
+    new_amount = round(c.convert(curr_from, curr_to, float(amount)), 2)
+    return new_amount
+
+def get_symbol(curr_to):
+    new_symbol = s.get_symbol(curr_to)
+    return new_symbol
+
+def check_currency_code(currency):
+    try: 
+        c.get_currency_name(currency)
+        return True
+    except: 
+        return False
+    
